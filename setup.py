d2df231b51b44e56ac3b40ec45d3b29424ae3556
Uncle_Jiu <107346913+Proviasw@users.noreply.github.com>
2023-01-26 17:23:56 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,34 @@
-from distutils.core import setup
-from setuptools import find_packages
-
-
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
-setup(name='nonebot-plugin-mcport',  
-      version='1.0.0',  
-      description='通过RCON向我的世界JAVA服务器发送命令',
-      author='Provias',
-      author_email='1686886163@qq.com',
-      url='https://github.com/Proviasw/nonebot-plugin-mcport',
-      license='MIT License',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      packages=find_packages(),
-      platforms=["all"],
-      classifiers=[
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Natural Language :: Chinese (Simplified)',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: Implementation',        
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Topic :: Software Development :: Libraries'
-      ],
-      python_requires='>=3.8',
-      install_requires=[
-        "nonebot2>=2.0.0rc1,<3.0.0",
-        "nonebot-adapter-onebot>=2.1.5"
-    ]
-)
+from setuptools import find_packages, setup
+import os
+path = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(path, "README.md"), "r", encoding="utf-8") as f:
+    long_description = f.read()
+    setup(name='nonebot-plugin-mcport',  # 包名
+          version='1.0.0',  # 版本号
+          description='通过RCON向我的世界JAVA服务器发送命令.',
+          long_description=long_description,
+          long_description_content_type="text/markdown",
+          author='Provias',
+          author_email='1686886163@qq.com',
+          url='http://www.jiushu.info',
+          include_package_data=True,
+          install_requires=[
+              "numpy",
+              "pillow",
+              "nonebot2>=2.0.0a16",
+              "nonebot-adapter-onebot>=2.0.0b1",
+              "nonebot-plugin-apscheduler>=0.2.0"
+              "async-mcrcon"
+          ],
+          license='MIT License',
+          packages=find_packages(),
+          platforms=["all"],
+          classifiers=['Intended Audience :: Developers',
+                       'Operating System :: OS Independent',
+                       'Natural Language :: Chinese (Simplified)',
+                       'Programming Language :: Python',
+                       'Programming Language :: Python :: 3.8',
+                       'Programming Language :: Python :: 3.9',
+                       'Topic :: Software Development :: Libraries'
+                       ],
+          )
